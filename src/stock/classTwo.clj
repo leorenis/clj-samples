2d2ed291c480c1075d64e0cfe1dcc41b599eb61e
leorenis <leorenis@gmail.com>
2020-06-10 09:29:52 -0300

    Learning clojure.

---
+++ src/stock/classTwo.clj
@@ -0,0 +1,21 @@
+(ns stock.classTwo)
+
+(defn print-message []
+  (println "### SystemX ###")
+  (println "Welcome to Storing."))
+
+
+(print-message)
+
+(defn ten-percent-discounted
+  "Returns the value with discount of 10% when raw value greater than 100."
+  [raw-value]
+  (if (> raw-value 100)
+    (let [discount-rate (/ 10 100)                        ;; Multiples declarations in let...
+          discount      (* raw-value discount-rate)]
+      (println "Calc discount of " discount)
+      (- raw-value discount))
+    raw-value))
+
+(ten-percent-discounted 1000)
+(ten-percent-discounted 100)
