b3e3015d1024b37c0d051d69b6ab3011d38cdff5
leorenis <leorenis@gmail.com>
2020-06-15 09:02:38 -0300

    Learning clojure.

--- src/stock/classFour.clj
+++ src/stock/classFour.clj
@@ -2,6 +2,7 @@
 
 (def prices [30 700 1000])                                  ; Define array with Three elements
 
-(println (prices 0))                                        ; Print first element of array
+(println (prices 0))                                        ; Print first element of array, using vector as function.
 (println (get prices 2))
 (println (get prices 4))                                    ; In this case, get do not return IndexOfBoundException. Returns nil.
+(println (get prices 4 0))                                  ; Defines default value, if index 4 doesn't exists, return 0 instead nil.
