d91baf50392174206b80992509fe7efd5ecf25eb
leorenis <leorenis@gmail.com>
2020-06-15 09:18:49 -0300

    Learning clojure.

--- src/stock/classFour.clj
+++ src/stock/classFour.clj
@@ -6,3 +6,12 @@
 (println (get prices 2))
 (println (get prices 4))                                    ; In this case, get do not return IndexOfBoundException. Returns nil.
 (println (get prices 4 0))                                  ; Defines default value, if index 4 doesn't exists, return 0 instead nil.
+(println (get prices 2 0))                                  ; When exists, returns the value in position.
+; TO avoid exceptions, use get function.
+
+(println (conj prices 10))                                  ; Return new array with element on the end.
+
+(println (+ 5 1))
+(println (inc 5))                                           ; inc function return the value + 1. Increase.
+(println (update prices 0 inc))
+(println (update prices 1 dec))
