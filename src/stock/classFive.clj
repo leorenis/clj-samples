5e04fe7bac66674917a3d6fe1427f41a87ce0a9e
leorenis <leorenis@gmail.com>
2020-06-19 06:00:56 -0300

    Learning clojure.

--- src/stock/classFive.clj
+++ src/stock/classFive.clj
@@ -1,12 +1,12 @@
 (ns stock.classFive)
 
-(def stock { :bag 10, :skirt 5})                            ; Using keyword as key
+(def stock { :bag 10, :skirt 5, :chair 3})                            ; Using keyword as key
 (println stock)
 (println "We have" (count stock) "elements")
 (println "Keys are:" (keys stock))
 (println "Values: " (vals stock))
 
-(println (assoc stock :chair 3))
+(println (assoc stock :sock 2))
 
 (defn less-one
   "Take less one."
@@ -16,3 +16,4 @@
 
 (println (update stock :bag less-one))
 (println (update stock :skirt #(- % 1)))
+(println (dissoc stock :sock))
