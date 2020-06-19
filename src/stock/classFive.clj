0f8a61d39d365b5df0b376a6679ef7a601511e3c
leorenis <leorenis@gmail.com>
2020-06-19 05:44:25 -0300

    Learning clojure.

--- src/stock/classFive.clj
+++ src/stock/classFive.clj
@@ -1,4 +1,7 @@
 (ns stock.classFive)
 
-(def stock { :bag 10 :skirt 5})
+(def stock { :bag 10, :skirt 5})
 (println stock)
+(println "We have" (count stock) "elements")
+(println "Keys are:" (keys stock))
+(println "Values: " (vals stock))
