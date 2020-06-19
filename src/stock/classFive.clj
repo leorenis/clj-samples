62788f9bade98d62c643ed3179191a5003eced51
leorenis <leorenis@gmail.com>
2020-06-19 05:47:17 -0300

    Learning clojure.

--- src/stock/classFive.clj
+++ src/stock/classFive.clj
@@ -1,6 +1,6 @@
 (ns stock.classFive)
 
-(def stock { :bag 10, :skirt 5})
+(def stock { :bag 10, :skirt 5})                            ; Using keyword as key
 (println stock)
 (println "We have" (count stock) "elements")
 (println "Keys are:" (keys stock))
