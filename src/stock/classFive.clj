4da0a3ee8ec320f544d78ac9b1f301a3d28e22a3
leorenis <leorenis@gmail.com>
2020-06-19 05:55:11 -0300

    Learning clojure.

--- src/stock/classFive.clj
+++ src/stock/classFive.clj
@@ -5,3 +5,14 @@
 (println "We have" (count stock) "elements")
 (println "Keys are:" (keys stock))
 (println "Values: " (vals stock))
+
+(println (assoc stock :chair 3))
+
+(defn less-one
+  "Take less one."
+  [value]
+  (println "Taking 1")
+  (- value 1))
+
+(println (update stock :bag less-one))
+(println (update stock :skirt #(- % 1)))
