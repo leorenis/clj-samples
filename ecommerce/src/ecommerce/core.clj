485b13cd4891326ea7b95af947a94186e8c19514
leorenis <leorenis@gmail.com>
2020-07-06 09:21:15 -0300

    Studying closure.

--- ecommerce/src/ecommerce/core.clj
+++ ecommerce/src/ecommerce/core.clj
@@ -1,6 +1,9 @@
 (ns ecommerce.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(map println ["John" "Doe" "Peter"])
+(println (first ["John" "Doe", "Peter"]))
+(println (rest ["John" "Doe", "Peter"]))
+(println (rest []))
+(println (next ["John" "Doe", "Peter"]))
+(println (next []))
+(println (seq []))
