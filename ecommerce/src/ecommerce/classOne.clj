19e4418215356e8bc27381747be18e8cc08d0056
leorenis <leorenis@gmail.com>
2020-07-15 23:03:38 -0300

    Studying clojure.

--- ecommerce/src/ecommerce/classOne.clj
+++ ecommerce/src/ecommerce/classOne.clj
@@ -1,13 +1,14 @@
 (ns ecommerce.classOne)
 
+; TAIL RECURSION
 (defn custom-map
   [func sequence]
   (let [f (first sequence)]
     (if (not (nil? f))
       (do
         (func f)
-        (recur func (rest sequence))))))                    ; recur fn must be the last stub of function. recur convert recursion in loop.
+        (recur func (rest sequence))))))                    ; recur fn must be the last stub of function before return. recur convert recursion in loop.
 
 (custom-map println ["John" "Doe" "Peter"])
 
-(custom-map println (range 100000))
+(custom-map println (range 10000))
