d22dc4041e5e52022d6f84a0c75ce242cdc1a8e9
leorenis <leorenis@gmail.com>
2020-07-17 23:10:45 -0300

    Studying clojure.

--- ecommerce/src/ecommerce/classTwo.clj
+++ ecommerce/src/ecommerce/classTwo.clj
@@ -7,3 +7,12 @@
     total))
 
 (println (custom-count 0 ["John" "Doe"]))
+
+; Test functions variations
+
+(defn my-func
+  ([parameter-one] (println "Single parameter" parameter-one))
+  ([parameter-one parameter-two] (println "Couple parameters" parameter-one parameter-two)))
+
+(my-func 1)
+(my-func 1 2)
