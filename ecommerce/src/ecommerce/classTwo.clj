d60979e5a22dc94b7bb513563e2221a04c79c1cb
leorenis <leorenis@gmail.com>
2020-07-17 23:16:15 -0300

    Studying clojure.

--- ecommerce/src/ecommerce/classTwo.clj
+++ ecommerce/src/ecommerce/classTwo.clj
@@ -16,3 +16,17 @@
 
 (my-func 1)
 (my-func 1 2)
+
+
+; Refactoring our function to use multiple polymorphic way...
+
+(defn custom-count
+  ([elements]
+   (custom-count 0 elements))
+  ([total elements]                                         ; two arity
+   (if (seq elements)
+     (recur (inc total) (next elements))
+     total)))
+
+(println (custom-count 0 ["John" "Doe"]))                   ; passing two parameters
+(println (custom-count []))                                 ;  passing only elements
