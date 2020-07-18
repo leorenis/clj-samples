be0b96e01c73f266e669347edfdd64a425ca8e1e
leorenis <leorenis@gmail.com>
2020-07-17 23:03:21 -0300

    Studying clojure.

--- ecommerce/src/ecommerce/classTwo.clj
+++ ecommerce/src/ecommerce/classTwo.clj
@@ -1,1 +1,9 @@
 (ns ecommerce.classTwo)
+
+(defn custom-count
+  [total elements]                                          ; two arity
+  (if (seq elements)
+    (recur (inc total) (next elements))
+    total))
+
+(println (custom-count 0 ["John" "Doe"]))
