54d1cd572fe139dfebcd3c968184ec637e105731
Léo Renis Santos <leorenis@gmail.com>
2020-10-01 21:15:44 -0300

    Update db.clj

--- ecommerce/src/ecommerce/db.clj
+++ ecommerce/src/ecommerce/db.clj
@@ -23,15 +23,15 @@
 (def order4 {:user 20
               :items {:bag {:id :bag :amount 6 :unit-price 40}
                       :shirt {:id :shirt :amount 8 :unit-price 85}
-                      :shoes {:id :shoes :amount 5 :unit-price 58}}})
+                      :shoes {:id :shoes :amount 4 :unit-price 58}}})
 
 (def order5 {:user 25
               :items {:bag {:id :bag :amount 7 :unit-price 60}
-                      :shirt {:id :shirt :amount 19 :unit-price 60.9}
+                      :shirt {:id :shirt :amount 29 :unit-price 60.9}
                       :shoes {:id :shoes :amount 20 :unit-price 69}}})
 
 (def order6 {:user 31
-              :items {:bag {:id :bag :amount 15 :unit-price 75}
+              :items {:bag {:id :bag :amount 15 :unit-price 76}
                       :shirt {:id :shirt :amount 12 :unit-price 100}
                       :shoes {:id :shoes :amount 8 :unit-price 84}}})
 
